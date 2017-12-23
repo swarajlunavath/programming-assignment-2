6ee9e242bc50a1ca70b8ed06c0ba96eb2189c968
swarajlunavath <markevenz@gmail.com>
2017-12-23 19:30:41 +0530

    first commit

---
+++ programming assignment2.R
@@ -0,0 +1,38 @@
+makeCacheMatrix <- function(x = numeric()) {
+          
+    
+            cache <- NULL
+         
+           setMatrix <- function(newValue) {
+                   x <<- newValue
+                   cache <<- NULL
+           }
+   
+           getMatrix <- function() {
+                  x
+          }
+        cacheInverse <- function(solve) {
+                   cache <<- solve
+           }
+   
+           getInverse <- function() {
+                   cache
+          }
+           
+           list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
+}
+
+
+cacheSolve <- function(y, ...) {
+          inverse <- y$getInverse()
+        if(!is.null(inverse)) {
+                  message("getting cached data")
+                  return(inverse)
+          }
+          data <- y$getMatrix()
+          inverse <- solve(data)
+          y$cacheInverse(inverse)
+          
+          # return the inverse
+          inverse
+  }
